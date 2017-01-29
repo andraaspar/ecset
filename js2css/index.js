8032496789011ec248648d5f1cd8ca5dfcec1655
András Parditka <andraaspar@gmail.com>
2017-01-29 13:07:10 +0100

    Added copyright notice.

--- js2css/index.js
+++ js2css/index.js
@@ -1,13 +1,32 @@
+/*
+ * Copyright 2017 András Parditka.
+ *
+ * This file is part of Ecset.
+ *
+ * Ecset is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ecset is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ecset.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 let out = ``
 
 let imports = [
 	'./vars',
 	'../bower_components/normalize-css/normalize.css',
 	'./elems',
-	'./regulars/p-canvas.js',
-	'./regulars/p-ecset.js',
-	'./regulars/p-path.js',
-	'./regulars/p-point.js',
+	'./regulars/p-canvas',
+	'./regulars/p-ecset',
+	'./regulars/p-path',
+	'./regulars/p-point',
 ]
 
 imports.filter(i => !i.endsWith('.css')).forEach(i => require(i))
